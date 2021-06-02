04315baacd0db7049c83c1d291ee5c5083cbb869
ClauCampos <82203430+clauCampos@users.noreply.github.com>
2021-06-02 17:46:45 +0200

    Add files via upload

---
+++ src/main/java/org/factoriaf5/libritos/controllers/BookController.java
@@ -0,0 +1,59 @@
+package org.factoriaf5.libritos.controllers;
+
+import org.factoriaf5.libritos.models.Book;
+import org.factoriaf5.libritos.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+
+@Controller
+public class BookController {
+
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/books")
+    String listBooks(Model model) {
+        List<Book> books = bookService.allBooks();
+        model.addAttribute("title", "Book list");
+        model.addAttribute("books", books);
+        return "books/all";
+    }
+
+    @GetMapping("/books/new")
+    String newBook(Model model) {
+            Book book = new Book();
+            model.addAttribute("book", book);
+            model.addAttribute("title", "Create new book");
+        return "books/new";
+            }
+    @GetMapping("books/edit/{id}")
+    String editBook(Model model, @PathVariable Long id){
+        Book book = bookService.findById(id);
+        model.addAttribute("book", book);
+        model.addAttribute("title", "Edit book");
+        return "books/edit";
+    }
+    @GetMapping("books/delete/{id}")
+    public String remove(@PathVariable Long id) {
+        bookService.delete(id);
+        return "redirect:/books";
+    }
+    @PostMapping("/books/new")
+    public String addBook(@ModelAttribute Book book) {
+        bookService.save(book);
+        return "redirect:/books";
+        }
+
+}
